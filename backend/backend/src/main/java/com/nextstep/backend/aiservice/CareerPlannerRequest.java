cd84ae29c6cab7a2b8f10c8db5df232a2aab3705
shrutisharma-sh <shrutish8439@gmail.com>
2026-08-05 22:40:45 +0530

    created dto 4 connecting fastapi career planner ffeature

---
+++ backend/backend/src/main/java/com/nextstep/backend/aiservice/CareerPlannerRequest.java
@@ -0,0 +1,14 @@
+package com.nextstep.backend.aiservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CareerPlannerRequest {
+    private String question;
+}
---
+++ backend/backend/src/main/java/com/nextstep/backend/aiservice/CareerPlannerResponse.java
@@ -0,0 +1,15 @@
+package com.nextstep.backend.aiservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CareerPlannerResponse {
+    private String question;
+    private String answer;
+}
